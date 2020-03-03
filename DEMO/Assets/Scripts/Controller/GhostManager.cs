90a4a3fcff1b0e010da712ac32f5202efb56ff08
lzw <729239776@qq.com>
2020-03-03 20:27:23 +0800

    幽灵逻辑

---
+++ DEMO/Assets/Scripts/Controller/GhostManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostManager : MonoBehaviour
+{
+    public struct GPos
+    {
+        public int number;
+        public Transform pos;
+        public bool isUsed;
+        public float timer;
+    }
+    [HideInInspector]
+    public GPos[] initialPos = new GPos[3];
+    [HideInInspector]
+    public static int ghostLife = 3;
+    public GameObject enemy;
+    public float reBornTime;
+
+    private GameObject[] gObj = new GameObject[3];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            gObj[i] = enemy.transform.GetChild(0).gameObject;
+            initialPos[i].number = i;
+            initialPos[i].pos = transform.GetChild(i);
+            initialPos[i].isUsed = false;
+            initialPos[i].timer = -1;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (var item in initialPos)
+        {
+            int i = item.number;
+            if (initialPos[i].timer >= 0 && initialPos[i].timer < reBornTime)
+                initialPos[i].timer += 0.1f;
+            else if (initialPos[i].timer >= reBornTime)
+                initialPos[i].isUsed = false;
+        }
+    }
+
+    public int ReBorn()
+    {
+        foreach (var item in initialPos)
+        {
+            if (!item.isUsed)
+                return item.number;
+        }
+
+        return 0;
+    }
+
+    public static GhostManager _instance;
+    public static GhostManager Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    public void Awake()
+    {
+        _instance = this;
+    }
+}
--- DEMO/Assets/Scripts/Enemy/Ghost.cs
+++ DEMO/Assets/Scripts/Enemy/Ghost.cs
@@ -5,12 +5,22 @@ using Pathfinding;
 
 public class Ghost : MonoBehaviour
 {
+    public enum Status
+    {
+        Attack, Escape, Die, None
+    }
+
     public float ghostType;
     public bool canDead;
+    //[HideInInspector]
+    public Status status = Status.Die;
 
     private AIPath aiPath;
     private Animator anim;
+
     private int dir = 0;
+    private int life;
+    private int iNum;
 
     // Start is called before the first frame update
     void Start()
@@ -18,12 +28,42 @@ public class Ghost : MonoBehaviour
         anim = GetComponent<Animator>();
         aiPath = GetComponent<AIPath>();
         anim.SetFloat("Type", ghostType);
+        life = GhostManager.ghostLife;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (life <= 0 && canDead)
+            status = Status.Die;
+
+        StatusController();
         MoveDir();
+    }
+
+    private void StatusController()
+    {
+        switch (status)
+        {
+            case Status.Attack:
+                GetComponent<AIPath>().enabled = true;
+                break;
+            case Status.Escape:
+                break;
+            case Status.Die:
+                GetComponent<AIPath>().enabled = false;
+                iNum = GhostManager.Instance.ReBorn();
+                transform.position = GhostManager.Instance.initialPos[iNum].pos.position;
+                GhostManager.Instance.initialPos[iNum].timer = 0;
+                GhostManager.Instance.initialPos[iNum].isUsed = true;
+                life = GhostManager.ghostLife;
+                status = Status.None;
+                break;
+            case Status.None:
+                if (!GhostManager.Instance.initialPos[iNum].isUsed)
+                    status = Status.Attack;
+                break;
+        }
     }
 
     private void MoveDir()
@@ -44,6 +84,7 @@ public class Ghost : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.tag == "Bullet")
+            life--;
     }
 }
