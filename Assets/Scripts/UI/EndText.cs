1fbb8498a50f8f57b5c6dc6d9cbc66cc3eb93681
lzw <729239776@qq.com>
2020-06-01 13:34:14 +0800

    end文字

---
+++ Assets/Scripts/UI/EndText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class EndText : MonoBehaviour
+{
+    private Transform people;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        people = transform.Find("Text");
+    }
+
+    // Update is called once per frame
+    void OnEnable()
+    {
+        people.DOLocalMoveY(1000, 10);
+    }
+}
